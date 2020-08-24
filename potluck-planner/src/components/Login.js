20e8c83cb86ba441e6dc552490ae7b02a84ac899
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-08-24 09:36:26 -0400

    fleshing out login form and setting some state to be used for it. installed router to use historypush. Still need some info about api to be able to make proper axios calls and set token

--- potluck-planner/src/components/Login.js
+++ potluck-planner/src/components/Login.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import axiosWithAuth from './utils/axiosWithAuth'
 
 export default function Login() {
+
+    const credentials = {
+        email: '',
+        password: ''
+    }
+    const [login, setLogin] = useState(credentials)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axiosWithAuth().post('apiurl', credentials)
+        // need to update api url to reflect backend address
+            .then(res => {
+                console.log(res)
+                localStorage.setItem('token', res.data)
+                // may need to change location of token from res
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    const handleChange = (e) => {
+
+    }
+
     return (
         <div>
-            Login Form
+            <form>
+
+            </form>
         </div>
     )
 }
