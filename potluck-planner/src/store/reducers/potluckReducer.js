1eb7ad0e1e0dc6aac915611f26b3107151f70aeb
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-08-25 10:31:06 -0400

    created potluckReducer and gave default case as well as some initial state

---
+++ potluck-planner/src/store/reducers/potluckReducer.js
@@ -0,0 +1,14 @@
+// import { SUCCSS, FAIL, LOADING} from '../actions'
+
+const initialState = {
+    potlucks: [],
+    loading: false,
+    error: ''
+}
+
+export const potluckReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
