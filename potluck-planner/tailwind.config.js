5e91ae936ea9d1a5e68a4a231e9c1348315587d3
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-08-25 12:02:08 -0400

    created custom color from theme in tailwind config file

--- potluck-planner/tailwind.config.js
+++ potluck-planner/tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
   purge: [],
   theme: {
     extend: {},
+    colors: {
+      orange: '#FF9F1C',
+      apricot: '#FFBF69',
+      white: '#FFFFFF',
+      cyan: '#CBF3F0',
+      aqua: '#2EC4B6'
+    }
   },
   variants: {},
   plugins: [],
