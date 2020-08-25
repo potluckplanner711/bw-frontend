9e62cbb453e15e8684754b455fe66064647bca3e
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-08-25 09:18:17 -0400

    minor style changes

--- potluck-planner/src/components/SignUp.js
+++ potluck-planner/src/components/SignUp.js
@@ -38,41 +38,57 @@ export default function SignUp() {
 
     return (
         <div>
-            <h2>Sign Up for Potluck Planner</h2>
-            <form onSubmit={handleSubmit}>
-            <input 
+            <form 
+                class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+                onSubmit={handleSubmit}>
+
+            <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2">
+            First Name
+                <input 
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     name='fname'
                     value={signup.fname}
                     type='text'
                     placeholder='First Name'
                     onChange={handleChange}
                 />
-                <br />
+            </label>
+            <label class="block text-gray-700 text-sm font-bold mb-2">
+            Last Name
                 <input 
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     name='lname'
                     value={signup.lname}
                     type='text'
                     placeholder='Last Name'
                     onChange={handleChange}
                 />
-                <br />
+            </label>
+            <label class="block text-gray-700 text-sm font-bold mb-2">
+            Email
                 <input 
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     name='email'
                     value={signup.email}
                     type='text'
                     placeholder='Email'
                     onChange={handleChange}
                 />
-                <br />
+            </label>
+            <label class="block text-gray-700 text-sm font-bold mb-2">
+            Password
                 <input 
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     name='password'
                     value={signup.password}
                     type='password'
                     placeholder='Password'
                     onChange={handleChange}
                 />
-                <br />
-                <button>Sign Up</button>
+            </label>
+            </div>
+                <button class='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>Sign Up</button>
             </form>
         </div>
     )
